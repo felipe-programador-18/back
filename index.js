02e2865b28eedbd9694d83a542a19d3e637926be
Felipe <machadofelipe2016@outlook.com>
2022-03-15 17:58:20 -0300

    c7- modify principles article

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express= require('express')
 const api = express()
 const port = process.env.Port || 3000
 const path = require('path')
-
+const User = require('./models/user')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/catalogo'
 const mongoose= require('mongoose')
 mongoose.Promise = global.Promise 
-//const User = require('./models/user')
+
 
 const produtos = require('./routes/produtos')
-const login = require('./routes/login')
+const home = require('./routes/home')
 
 
 api.use(session({secret:"testando se esta logado"}))
@@ -20,43 +20,39 @@ api.use(session({secret:"testando se esta logado"}))
 api.set('views', path.join(__dirname, 'views'))
 api.set('view engine','ejs')
 api.use(express.static(path.join( __dirname ,'views')))
+
 api.use(bodyParser.urlencoded({extended:true}))
 
 
 api.get('/', (req, res) =>{res.render('home')})
+
+api.use('/produtos',produtos)
 api.get('/produtos',(req,res, next ) => { 
-  res.redirect('/')
   if('user' in session){
     return next()  
   }
-  res.render('produtos')
+  res.redirect('/login')
+  
 })
 
-const User = require('./models/user')
-api.post('/login', async (req,res) => {
- const user = await User.findOne({
-     username : req.body.username
- }) 
- res.send(user)
-})
+api.use('/login', home)
 api.get('/login', (req, res) =>{res.render('login')})
-api.get('/login',(req,res ) => { 
-    //res.redirect('/')
-    //if('user' in session){
-     // return next()  
-   // }
-    res.render('login')
-  })
+api.post('/login', async(req,res) => {
+    const user = await User.findOne({username: req.body.username}) 
+    //const isValed = await user.checkPassword(req.body.password)
+    res.send(user)
+       //isValed
+        
+   })
 
-api.use('/login', login)
-api.use('/produtos',produtos)
+
 
 const CreateInitialuser =  async() => {
     const total = await User.count({username:'felipe programmer'})
     if(total === 0){
         const user = new User ({
             username:'felipe programmer',
-            email:'machadofeipe2015@outlook.com.br',
+            email:'machadofelipe2016@outlook.com',
             password:'432423marrtints',
         })
        await user.save()
